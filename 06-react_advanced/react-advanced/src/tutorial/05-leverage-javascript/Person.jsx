13a0ebea6f3d073b334fe778edce6cdbc4ce1fda
Adriano Ayala <adriano.ayc@gmail.com>
2024-06-28 01:44:33 -0400

    Leverage JavaScript - Complete

--- 06-react_advanced/react-advanced/src/tutorial/05-leverage-javascript/Person.jsx
+++ 06-react_advanced/react-advanced/src/tutorial/05-leverage-javascript/Person.jsx
@@ -1,5 +1,10 @@
-export function Person({ name, nickName, images }) {
-  const img = images[0].small.url;
+import avatar from '../../assets/default-avatar.svg';
+
+export function Person({ name, nickName = 'ShakeAndBake', images }) {
+  // const img = images && images[0] && images[0].small && images[0].small.url;
+  // const img = images?.[0]?.small?.url || avatar;
+  const img = images?.[0]?.small?.url ?? avatar;
+
   return (
     <div>
       <img src={img} alt={name} style={{ width: '50px' }} />
