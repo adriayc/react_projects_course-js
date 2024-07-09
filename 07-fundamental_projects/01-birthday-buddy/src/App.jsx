416cc439bad2e37fbc903dbd238ecc8449aef53b
Adriano Ayala <adriano.ayc@gmail.com>
2024-07-09 11:43:21 -0400

    Birthday Buddy - Import List

--- 07-fundamental_projects/01-birthday-buddy/src/App.jsx
+++ 07-fundamental_projects/01-birthday-buddy/src/App.jsx
@@ -1,4 +1,11 @@
+import data from './data';
+
+import { useState } from 'react';
+
 const App = () => {
+  const [people, setPeople] = useState(data);
+  console.log(people);
+
   return <h2>Birthday Reminder</h2>;
 };
 
