552de8fdbc1329226691e57c5646a6ca68264637
Adriano Ayala <adriano.ayc@gmail.com>
2024-07-09 11:57:31 -0400

    Birthday Buddy - Clear List

--- 07-fundamental_projects/01-birthday-buddy/src/App.jsx
+++ 07-fundamental_projects/01-birthday-buddy/src/App.jsx
@@ -11,6 +11,14 @@ const App = () => {
       <section className="container">
         <h3>{people.length} birthday today</h3>
         <List people={people} />
+
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          Clear all
+        </button>
       </section>
     </main>
   );
