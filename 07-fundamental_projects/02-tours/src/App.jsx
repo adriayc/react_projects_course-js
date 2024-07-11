574b2c01a12fa6045e772bb99377492caa3c67c4
Adriano Ayala <adriano.ayc@gmail.com>
2024-07-11 15:54:27 -0400

    Tours - Fetch Tours

--- 07-fundamental_projects/02-tours/src/App.jsx
+++ 07-fundamental_projects/02-tours/src/App.jsx
@@ -1,4 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const url = 'https://www.course-api.com/react-tours-project';
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return <h2>Tours</h2>;
 };
 
