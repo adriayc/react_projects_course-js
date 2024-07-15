d819ad8596d71a8e0e055f77e81b716a822b9d82
Adriano Ayala <adriano.ayc@gmail.com>
2024-07-15 18:32:22 -0400

    Reviews - First Person

--- 07-fundamental_projects/03-reviews/src/App.jsx
+++ 07-fundamental_projects/03-reviews/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import people from './data';
-import { FaBeer } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const App = () => {
   const [index, setIndex] = useState(0);
@@ -8,10 +8,19 @@ const App = () => {
   // console.log(name);
 
   return (
-    <div>
-      <h2>Reviews</h2>
-      <FaBeer className="beer" />
-    </div>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
   );
 };
 
