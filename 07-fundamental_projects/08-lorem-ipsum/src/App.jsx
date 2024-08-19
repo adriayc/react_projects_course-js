4516f5f0e0276bbcf42182f161e31d7616faedbb
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-19 18:07:08 -0400

    Lorem Ipsum - Nanoid Library

--- 07-fundamental_projects/08-lorem-ipsum/src/App.jsx
+++ 07-fundamental_projects/08-lorem-ipsum/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import data from './data';
 
 const App = () => {
@@ -35,8 +36,9 @@ const App = () => {
       </form>
 
       <article className="lorem-text">
-        {text.map((item, index) => {
-          return <p key={index}>{item}</p>;
+        {/* {text.map((item, index) => { */}
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
         })}
       </article>
     </section>
