c27ad48dc5e6d494d558268ad091dbe765d1434b
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-19 15:48:28 -0400

    Lorem Ipsum - Submit Form

--- 07-fundamental_projects/08-lorem-ipsum/src/App.jsx
+++ 07-fundamental_projects/08-lorem-ipsum/src/App.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+
+      <form className="lorem-form">
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default App;
