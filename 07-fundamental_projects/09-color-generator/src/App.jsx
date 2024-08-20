000e5f61274abe8dc6148318605563397becb8f7
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-20 17:41:06 -0400

    Color Generator - React Toastify

--- 07-fundamental_projects/09-color-generator/src/App.jsx
+++ 07-fundamental_projects/09-color-generator/src/App.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import Values from 'values.js';
+import { ToastContainer, toast } from 'react-toastify';
 import ColorList from './ColorList';
 import Form from './Form';
-import { useState } from 'react';
 
 const App = () => {
   // const [colors, setColors] = useState([]);
   const [colors, setColors] = useState(new Values('#f15025').all(10));
   // console.log(new Values('#f15025').all(10));
 
+  // Toastify
+  // toast.error('Error ');
+  // toast.success('Success');
+
   return (
     <main>
       <Form />
       <ColorList colors={colors} />
+
+      {/* Toastify */}
+      <ToastContainer position="top-center" />
     </main>
   );
 };
