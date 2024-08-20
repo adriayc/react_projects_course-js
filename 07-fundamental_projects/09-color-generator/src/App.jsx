5046445802c678703899430a91911a2498221f71
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-20 17:11:24 -0400

    Color Generator - Values.js Library

--- 07-fundamental_projects/09-color-generator/src/App.jsx
+++ 07-fundamental_projects/09-color-generator/src/App.jsx
@@ -1,7 +1,13 @@
+import Values from 'values.js';
 import ColorList from './ColorList';
 import Form from './Form';
+import { useState } from 'react';
 
 const App = () => {
+  // const [colors, setColors] = useState([]);
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
+  // console.log(new Values('#f15025').all(10));
+
   return (
     <main>
       <Form />
