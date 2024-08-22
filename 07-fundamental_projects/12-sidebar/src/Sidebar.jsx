5655276fe789837f416418c56abf2d396c19ef82
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-21 21:47:15 -0400

    Sidebar/Modal - Sidebar Component JSX

--- 07-fundamental_projects/12-sidebar/src/Sidebar.jsx
+++ 07-fundamental_projects/12-sidebar/src/Sidebar.jsx
@@ -1,5 +1,46 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+import { links, social } from './data';
+import logo from './logo.svg';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="Logo" className="logo" />
+        <button type="button" className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={text}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
