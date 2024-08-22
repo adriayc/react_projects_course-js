a52cab2fc64c5eb60c33866743ccd3d82b397d24
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-21 21:45:32 -0400

    Sidebar/Modal - Home Component

--- 07-fundamental_projects/12-sidebar/src/Home.jsx
+++ 07-fundamental_projects/12-sidebar/src/Home.jsx
@@ -1,10 +1,19 @@
 import { useGlobalContext } from './context';
+import { FaBars } from 'react-icons/fa';
 
 const Home = () => {
   const { openSidebar, openModal } = useGlobalContext();
-  console.log(openSidebar);
 
-  return <div>Home</div>;
+  return (
+    <main>
+      <button type="button" className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button type="button" className="btn" onClick={openModal}>
+        Show modal
+      </button>
+    </main>
+  );
 };
 
 export default Home;
