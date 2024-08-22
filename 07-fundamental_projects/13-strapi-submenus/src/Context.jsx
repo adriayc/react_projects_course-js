dc6db48e363d9ffcb9570e320aefb45da6e782cc
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-22 12:16:16 -0400

    Strapi - Global Context

---
+++ 07-fundamental_projects/13-strapi-submenus/src/Context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from 'react';
+
+// Define context
+const AppContext = createContext();
+
+// Create provider
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// Custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
