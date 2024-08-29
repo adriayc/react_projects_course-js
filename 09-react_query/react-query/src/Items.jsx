22b704fd72ca6a20c82395345adab7008db6b7b5
Adriano Ayala <adriano.ayc@gmail.com>
2024-08-29 15:58:56 -0400

    First Query

--- 09-react_query/react-query/src/Items.jsx
+++ 09-react_query/react-query/src/Items.jsx
@@ -1,6 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
 import SingleItem from './SingleItem';
 
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/'),
+  });
+  console.log(result);
+
   return (
     <div className="items">
       {items.map((item) => {
