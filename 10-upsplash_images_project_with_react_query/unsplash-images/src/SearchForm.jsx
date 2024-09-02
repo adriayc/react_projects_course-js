d5a3615f393ff6f337bc90cc90d5951403399e2b
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-02 17:03:11 -0400

    SearchForm

--- 10-upsplash_images_project_with_react_query/unsplash-images/src/SearchForm.jsx
+++ 10-upsplash_images_project_with_react_query/unsplash-images/src/SearchForm.jsx
@@ -1,5 +1,30 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.elements);
+
+    // Get and validate value
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">Unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
