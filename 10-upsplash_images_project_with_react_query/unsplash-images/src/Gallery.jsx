5b14a0668a9667dc97f74d20424f1fc0e226a02b
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-03 16:11:38 -0400

    Setup Query

--- 10-upsplash_images_project_with_react_query/unsplash-images/src/Gallery.jsx
+++ 10-upsplash_images_project_with_react_query/unsplash-images/src/Gallery.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url =
+  'https://api.unsplash.com/search/photos?client_id={{YOUR_ACCESS_KEY}}&query=cat';
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
+
   return <h2>Gallery</h2>;
 };
 
