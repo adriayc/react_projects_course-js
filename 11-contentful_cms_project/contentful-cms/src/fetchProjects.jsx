92aba54627d331155eeb782198139523e57d76f1
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-05 23:41:48 -0400

    Fetch Projects

--- 11-contentful_cms_project/contentful-cms/src/fetchProjects.jsx
+++ 11-contentful_cms_project/contentful-cms/src/fetchProjects.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { createClient } from 'contentful';
+import { useEffect } from 'react';
 
 const client = createClient({
   space: '{{SPACE_ID}}',
@@ -6,8 +8,39 @@ const client = createClient({
   accessToken: '{{ACCESS_TOKEN}}',
 });
 
-client
-  .getEntries({
-    content_type: 'projects',
-  })
-  .then((response) => console.log(response));
+// client
+//   .getEntries({
+//     content_type: 'projects',
+//   })
+//   .then((response) => console.log(response));
+
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+      const projects = response.items.map((item) => {
+        // console.log(item);
+        const id = item.sys.id;
+        const { title, url, image } = item.fields;
+        const img = image?.fields?.file?.url;
+
+        return { id, title, url, img };
+      });
+
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { loading, projects };
+};
