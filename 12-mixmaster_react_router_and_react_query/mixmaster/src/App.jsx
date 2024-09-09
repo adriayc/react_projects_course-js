08c9e9ab69b1dbff951d7473ca42385f6752e356
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-09 17:38:12 -0400

    Basic Router

--- 12-mixmaster_react_router_and_react_query/mixmaster/src/App.jsx
+++ 12-mixmaster_react_router_and_react_query/mixmaster/src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <h2>Home page</h2> },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>About page</h2>
+      </div>
+    ),
+  },
+]);
+
 const App = () => {
-  return <h2>MixMaster</h2>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
