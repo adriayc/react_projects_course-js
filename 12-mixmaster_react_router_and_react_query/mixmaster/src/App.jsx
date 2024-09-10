a087fc0336b07121cbe6799b875ecf782cd732bb
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-10 18:02:13 -0400

    Single Page Error

--- 12-mixmaster_react_router_and_react_query/mixmaster/src/App.jsx
+++ 12-mixmaster_react_router_and_react_query/mixmaster/src/App.jsx
@@ -7,6 +7,7 @@ import {
   Cocktail,
   Landing,
   Error,
+  SinglePageError,
 } from './pages';
 
 import { loader as landingLoader } from './pages/Landing';
@@ -20,6 +21,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        // errorElement: <h2>Something went wrong...</h2>,
+        errorElement: <SinglePageError />,
         loader: landingLoader,
       },
       {
