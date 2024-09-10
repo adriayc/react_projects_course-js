c7a1d478447ced489aa2d6dd29db579278a4a642
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-10 12:09:42 -0400

    Navbar

--- 12-mixmaster_react_router_and_react_query/mixmaster/src/pages/HomeLayout.jsx
+++ 12-mixmaster_react_router_and_react_query/mixmaster/src/pages/HomeLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
+// Components
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>Navbar</nav>
+    <>
+      <Navbar />
       <Outlet />
-    </div>
+    </>
   );
 };
 
