37e9c70bb5251eac62975e5564d487a8e7f3b7fc
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-11 18:01:43 -0400

    Single Cocktail - Setup

--- 12-mixmaster_react_router_and_react_query/mixmaster/src/App.jsx
+++ 12-mixmaster_react_router_and_react_query/mixmaster/src/App.jsx
@@ -10,7 +10,9 @@ import {
   SinglePageError,
 } from './pages';
 
+// Loaders
 import { loader as landingLoader } from './pages/Landing';
+import { loader as singleCocktailLoader } from './pages/Cocktail';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +30,8 @@ const router = createBrowserRouter([
       {
         path: 'cocktail/:id',
         element: <Cocktail />,
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader,
       },
       {
         path: 'newsletter',
--- 12-mixmaster_react_router_and_react_query/mixmaster/src/pages/Cocktail.jsx
+++ 12-mixmaster_react_router_and_react_query/mixmaster/src/pages/Cocktail.jsx
@@ -1,4 +1,24 @@
+import axios from 'axios';
+import { useLoaderData, Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/CocktailPage';
+
+const singleCocktailUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+// export const loader = async (data) => {
+export const loader = async ({ params }) => {
+  // console.log(data);
+  const { id } = params;
+
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+  // console.log(data);
+
+  return { id, data };
+};
+
 const Cocktail = () => {
+  const { id, data } = useLoaderData();
+
   return <h1>Cocktail</h1>;
 };
 
