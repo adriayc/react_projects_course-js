b009ad541ee7938cf2abb0b160a34cbb4ece7361
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-11 23:40:42 -0400

    Newsletter Structure

--- 12-mixmaster_react_router_and_react_query/mixmaster/src/pages/Newsletter.jsx
+++ 12-mixmaster_react_router_and_react_query/mixmaster/src/pages/Newsletter.jsx
@@ -1,5 +1,55 @@
 const Newsletter = () => {
-  return <h1>Newsletter</h1>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        Our newsletter
+      </h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          defaultValue="john"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          Last name
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="form-input"
+          defaultValue="smith"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="form-input"
+          defaultValue="test@test.com"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: '0.5rem' }}
+      >
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Newsletter;
