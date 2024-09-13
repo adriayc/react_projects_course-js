80ad17fd4623c02f98602cbb720666374a7e2523
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-13 18:27:57 -0400

    Skills Section

--- 13-webdev_portfolio_tailwind_css/tailwind-portfolio/src/App.jsx
+++ 13-webdev_portfolio_tailwind_css/tailwind-portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 // Components
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
     </>
   );
 };
