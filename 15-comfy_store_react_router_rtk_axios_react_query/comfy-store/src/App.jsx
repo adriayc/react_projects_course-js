682d213004cfdd561d8d57c748aabf86e1d2690a
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-20 17:57:55 -0400

    Create Pages

--- 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/App.jsx
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/App.jsx
@@ -1,3 +1,18 @@
+// Pages
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
 const App = () => {
   return <h1 className="text-3xl font-bold underline">Comfy Store</h1>;
 };
