a884c73cfb186e795107e58daf2623b7666c8a7f
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-23 23:57:33 -0400

    Toggle Component

--- 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/components/Navbar.jsx
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import { BsCart3 } from 'react-icons/bs';
+import { useState } from 'react';
+import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 // Components
 import NavLinks from './NavLinks';
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(false);
+
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
@@ -35,6 +42,13 @@ const Navbar = () => {
         </div>
         <div className="navbar-end">
           {/* THEME SETUP */}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            {/* Sun icon */}
+            <BsSunFill className="swap-on h-4 w-4" />
+            {/* Moon icon */}
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
           {/* CART LINK */}
           <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
             <div className="indicator">
