0d69b07d9a0725cf142ef78f27779d8471c1a21e
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-02 00:06:22 -0400

    Register User - Complete

--- 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/pages/Register.jsx
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/pages/Register.jsx
@@ -1,9 +1,25 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
 // Components
 import { FormInput, SubmitBtn } from '../components';
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
 
-export const action = async () => {
-  return null;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post('/auth/local/register', data);
+    toast.success('Account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    // console.log(error);
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'Please double check your credentials';
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
@@ -14,9 +30,24 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput type="text" label="Username" name="username" />
-        <FormInput type="email" label="Email" name="email" />
-        <FormInput type="password" label="Password" name="password" />
+        <FormInput
+          type="text"
+          label="Username"
+          name="username"
+          // defaultValue="adriano"
+        />
+        <FormInput
+          type="email"
+          label="Email"
+          name="email"
+          // defaultValue="adriano@email.com"
+        />
+        <FormInput
+          type="password"
+          label="Password"
+          name="password"
+          // defaultValue="adriano123"
+        />
 
         <div className="mt-4">
           <SubmitBtn text="Register" />
