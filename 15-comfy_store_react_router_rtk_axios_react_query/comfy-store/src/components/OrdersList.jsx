6c4cc8eb1fb0ddaaa52c77cf42cb59945591f52a
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-03 17:12:21 -0400

    Orders Page - Setup

---
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/components/OrdersList.jsx
@@ -0,0 +1,5 @@
+const OrdersList = () => {
+  return <h1 className="text-4xl">OrdersL ist</h1>;
+};
+
+export default OrdersList;
--- 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/pages/Orders.jsx
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/pages/Orders.jsx
@@ -1,3 +1,46 @@
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+// Utils
+import { customFetch } from '../utils';
+
+// Loader
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const user = store.getState().userState.user;
+
+    if (!user) {
+      toast.warn('You must logged in to view orders');
+      return redirect('/login');
+    }
+    // Get params
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    console.log(params);
+
+    try {
+      const response = await customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'There was an error placing your order';
+      // Toastify
+      toast.error(errorMessage);
+      if (error.response.status === 401 || error.response.status === 403)
+        return redirect('/login');
+    }
+    return null;
+  };
+
 const Orders = () => {
   return <h1 className="text-4xl">Orders</h1>;
 };
