1ba28632b5f1c7f3563bbdbb9c3b35df3351aaa2
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-03 01:31:09 -0400

    Checkout Form - Complete

--- 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/components/CheckoutForm.jsx
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/components/CheckoutForm.jsx
@@ -1,13 +1,57 @@
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
+// Utils
+import { customFetch, formatPrice } from '../utils';
+// Actions (Redux)
+import { clearCart } from '../features/cart/cartSlice';
 // Components
 import FormInput from './FormInput';
 import SubmitBtn from './SubmitBtn';
+import { toast } from 'react-toastify';
 
 // Action
-export const action = (store) => async () => {
-  console.log(store);
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, address } = Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+
+    // Order data
+    const info = {
+      name,
+      address,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+
+    try {
+      const response = await customFetch.post(
+        '/orders',
+        { data: info },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      store.dispatch(clearCart());
+      // Toastify
+      toast.success('Order placed successfully');
+      return redirect('/orders');
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'There was an error placing your order';
+      // Toastify
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const CheckoutForm = () => {
   return (
