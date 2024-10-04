d14195d23b4c44cdffa8e18e2df5aaa43d5f087f
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-04 15:52:06 -0400

    React Query - All Products Page

--- 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/pages/Products.jsx
+++ 15-comfy_store_react_router_rtk_axios_react_query/comfy-store/src/pages/Products.jsx
@@ -4,6 +4,25 @@ import { Filters, PaginationContainer, ProductsContainer } from '../components';
 
 const url = '/products';
 
+// Query
+const allProductsQuery = (queryParams) => {
+  const { search, category, company, sort, price, shipping, page } =
+    queryParams;
+  return {
+    queryKey: [
+      'products',
+      search ?? '',
+      category ?? 'all',
+      company ?? 'all',
+      sort ?? 'a-z',
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () => customFetch.get('/products', { params: queryParams }),
+  };
+};
+
 // Loader
 export const loader =
   (queryClient) =>
@@ -16,7 +35,9 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
     ]);
 
-    const response = await customFetch.get(url, { params });
+    const response = await queryClient.ensureQueryData(
+      allProductsQuery(params)
+    );
     // const products = response.data.data;
     // const meta = response.data.meta;
     const { data: products, meta } = response.data;
