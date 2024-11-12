a8110efe33da2808d89c3e46c162636d4b1f95c9
Adriano Ayala <adriano.ayc@gmail.com>
2024-11-12 12:50:18 -0400

    Tasks Project - Second Part

--- 16-typescript_fundamentals/typescript-tutorial/src/tasks.ts
+++ 16-typescript_fundamentals/typescript-tutorial/src/tasks.ts
@@ -1,5 +1,7 @@
 // console.log('tasks');
 
+import { boolean } from 'zod';
+
 // const btn = document.querySelector<HTMLButtonElement>('.test-btn');
 // const btn = document.querySelector<HTMLButtonElement>('.test-btn')!;
 // const btn = document.querySelector('.test-btn')! as HTMLButtonElement;
@@ -22,28 +24,80 @@ type Task = {
   isCompleted: boolean;
 };
 
-const tasks: Task[] = [];
+// const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
 
-// taskForm?.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const taskDescription = formInput?.value;
-//   if (taskDescription) {
-//     console.log(taskDescription);
-//     formInput.value = '';
-//     return;
-//   }
-//   alert('Please enter a task description');
-// });
+// Get LS
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
 
-function createTask(event: SubmitEvent) {
+// Iterate  and render task
+tasks.forEach(renderTask);
+
+taskForm?.addEventListener('submit', (event) => {
   event.preventDefault();
   const taskDescription = formInput?.value;
   if (taskDescription) {
-    console.log(taskDescription);
+    // console.log(taskDescription);
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    // Add task to list
+    addTask(task);
+    // Render tasks
+    renderTask(task);
+    // Update local storage
+    updateStorage();
+
     formInput.value = '';
     return;
   }
   alert('Please enter a task description');
+});
+
+// function createTask(event: SubmitEvent) {
+//   event.preventDefault();
+//   const taskDescription = formInput?.value;
+//   if (taskDescription) {
+//     // console.log(taskDescription);
+//     formInput.value = '';
+//     return;
+//   }
+//   alert('Please enter a task description');
+// }
+
+// taskForm?.addEventListener('submit', createTask);
+
+// Add task
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
 }
 
-taskForm?.addEventListener('submit', createTask);
+// Render task
+function renderTask(task: Task): void {
+  const taskElement = document.createElement('li');
+  taskElement.textContent = task.description;
+
+  // Checkbox
+  const taskCheckbox = document.createElement('input');
+  taskCheckbox.type = 'checkbox';
+  taskCheckbox.checked = task.isCompleted;
+
+  // Toggle checkbox
+  taskCheckbox.addEventListener('change', () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+
+  taskElement.append(taskCheckbox);
+  taskListElement?.appendChild(taskElement);
+}
+
+// Set LS
+function updateStorage(): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
